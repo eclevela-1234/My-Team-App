2136926de7e78b66fcb9d3ee890af7c9546f565d
Eliot Cleveland <eclevela@richmond.edu>
2022-10-02 00:03:00 -0400

    added model files

---
+++ index.js
@@ -0,0 +1,19 @@
+const inquirer = require("inquirer");
+
+const promptInfo = () => {
+    console.log(`
+    ==========================
+    Welcome to My-Team-Builder
+    ==========================
+    `);
+    return inquirer.prompt([
+        {
+            type: "list",
+            name: "Input",
+            message: "Input something"
+        }
+    ])
+    .then(data => console.log(data.Input))
+}
+
+promptInfo();
