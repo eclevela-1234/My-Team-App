7e9f78384f8e8ec92ae4b1d0d4d0bcfca4e5d955
Eliot Cleveland <eclevela@richmond.edu>
2022-10-04 23:43:13 -0400

    set up employees object, playing with oop

--- index.js
+++ index.js
@@ -22,15 +22,21 @@
 
 // new App().start();
 
-const Employee =  require('./lib/Employee')
+const Employee =  require('./lib/Employee');
+const Manager = require('./lib/Manager');
 
-const bob = new Employee({name: "Bob", id: 1, email: "bob@email.com"});
+const bob = new Employee("Bob", 1, "bob@email.com");
 
+const eliot = new Manager('Eliot', 123)
 
+eliot.id = 1;
+eliot.email = "eliot@email.com";
 
 // bob.name = "Bob";
 // bob.id = 1;
 // bob.email = "bob@email.com";
 
 // bob = new Employee;
-console.log(bob.getName())
+console.log(bob);
+console.log(eliot);
+console.log(eliot.getRole());
